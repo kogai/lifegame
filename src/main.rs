d4fb71c5c3595ae4db8ede1000b2a76fc03fc426
小飼慎一 <kogai0121@gmail.com>
2017-05-18 22:20:41 +0900

    determine initial state

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,10 @@ mod world;
 use world::World;
 
 fn main() {
-    let game = World::new(10, 10);
+    let game = World::new(10, 10, vec![(4, 4), (4, 5), (5, 4), (5, 5)]);
+
+    println!("{}", game.show());
+
+    game.tick();
     println!("{}", game.show());
 }
--- src/world.rs
+++ src/world.rs
@@ -31,8 +31,27 @@ pub struct World {
 }
 
 impl World {
-    pub fn new(x: i32, y: i32) -> Self {
-        World { grid: (0..y).map(|_| (0..x).map(|_| Cell::death()).collect()).collect::<Vec<_>>() }
+    pub fn new(column: i32, row: i32, initial_state: Vec<(i32, i32)>) -> Self {
+        World {
+            grid: (0..row)
+                .map(|y| {
+                    (0..column)
+                        .map(|x| {
+                            for &(initial_x, initial_y) in &initial_state {
+                                if initial_x == x && initial_y == y {
+                                    return Cell::alive();
+                                }
+                            }
+                            Cell::death()
+                        })
+                        .collect()
+                })
+                .collect::<Vec<_>>(),
+        }
+    }
+
+    pub fn tick(&self) {
+        unimplemented!();
     }
 
     pub fn show(&self) -> String {
