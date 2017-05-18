ce151c5ccaac91906532f73cb91bc27be5360802
小飼慎一 <kogai0121@gmail.com>
2017-05-18 22:46:55 +0900

    impl tick

--- src/main.rs
+++ src/main.rs
@@ -2,10 +2,12 @@ mod world;
 use world::World;
 
 fn main() {
-    let game = World::new(10, 10, vec![(4, 4), (4, 5), (5, 4), (5, 5)]);
+    let mut game = World::new(10, 10, vec![(4, 4), (4, 5), (5, 4), (5, 5)]);
 
     println!("{}", game.show());
+    println!("------");
 
     game.tick();
     println!("{}", game.show());
+    println!("------");
 }
--- src/world.rs
+++ src/world.rs
@@ -1,27 +1,40 @@
 pub type Matrix<T> = Vec<Vec<T>>;
+type Position = (i32, i32);
 
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 pub enum Cell {
-    Alive,
-    Death,
+    Alive(Position),
+    Death(Position),
 }
 
 impl Cell {
-    fn alive() -> Self {
-        Cell::Alive
+    fn alive(x: Position) -> Self {
+        Cell::Alive(x)
     }
 
-    fn death() -> Self {
-        Cell::Death
+    fn death(x: Position) -> Self {
+        Cell::Death(x)
     }
 
     fn show(&self) -> String {
         use self::Cell::*;
         match self {
-                &Alive => "1",
-                &Death => "0",
+                &Alive(_) => "1",
+                &Death(_) => "0",
             }
             .to_owned()
+    }
+
+    fn should_live(&self, grid: &Matrix<Cell>) -> bool {
+        use self::Cell::*;
+        match self {
+            &Alive((x, y)) |
+            &Death((x, y)) => {
+                // let xx = &grid[1];
+                true
+            },
+        };
+        unimplemented!();
     }
 }
 
@@ -31,7 +44,7 @@ pub struct World {
 }
 
 impl World {
-    pub fn new(column: i32, row: i32, initial_state: Vec<(i32, i32)>) -> Self {
+    pub fn new(column: i32, row: i32, initial_state: Vec<Position>) -> Self {
         World {
             grid: (0..row)
                 .map(|y| {
@@ -39,19 +52,38 @@ impl World {
                         .map(|x| {
                             for &(initial_x, initial_y) in &initial_state {
                                 if initial_x == x && initial_y == y {
-                                    return Cell::alive();
+                                    return Cell::alive((x, y));
                                 }
                             }
-                            Cell::death()
+                            Cell::death((x, y))
                         })
                         .collect()
                 })
-                .collect::<Vec<_>>(),
+                .collect(),
         }
     }
 
-    pub fn tick(&self) {
-        unimplemented!();
+    pub fn tick(&mut self) {
+        let grid = &self.grid.clone();
+
+        self.grid = self.grid
+            .iter()
+            .map(|y| {
+                y.iter()
+                    .map(|x| if x.should_live(grid) {
+                        match x {
+                            &Cell::Alive(cell) |
+                            &Cell::Death(cell) => Cell::alive(cell),
+                        }
+                    } else {
+                        match x {
+                            &Cell::Alive(cell) |
+                            &Cell::Death(cell) => Cell::death(cell),
+                        }
+                    })
+                    .collect::<Vec<_>>()
+            })
+            .collect::<Vec<_>>();
     }
 
     pub fn show(&self) -> String {
